4f3bffa52bc6ab700d28e07e6b24f3e7fcae0b62
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-08-13 09:22:58 -0500

    api movies

--- src/main/java/MovieServlet.java
+++ src/main/java/MovieServlet.java
@@ -1,5 +1,7 @@
 import com.google.gson.Gson;
+import data.DaoFactory;
 import data.Movie;
+import data.MoviesDao;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(name = "MovieServlet", urlPatterns = "/movies")
 public class MovieServlet extends HttpServlet {
@@ -28,37 +31,34 @@ public class MovieServlet extends HttpServlet {
                     "Biography, Crime, Drama",
                     "1990"
             );
-            String movieString = new Gson().toJson(movie);
+            MoviesDao moviesDao = DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY);
+            //turn into a json string
+            String movieString = new Gson().toJson(moviesDao.all());
+            //inject into response
             out.println(movieString);
 
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+
         }
 
     }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("application/json");
         PrintWriter out = null;
+
         try {
             out = response.getWriter();
             Movie[] movies = new Gson().fromJson(request.getReader(), Movie[].class);
-//            for (Movie movie : movies) {
-//                System.out.println(movie.getId());
-//                System.out.println(movie.getTitle());
-//                System.out.println(movie.getDirector());
-//                System.out.println(movie.getPlot());
-//                System.out.println(movie.getPoster());
-//                System.out.println(movie.getActors());
-//                System.out.println(movie.getGenre());
-//                System.out.println(movie.getYear());
-//                System.out.println(movie.getRating());
-//                System.out.println("========================================================");
-//            }
-
+            DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY).insert(movies[0]);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            out.println(new Gson().toJson(e.getLocalizedMessage()));
+            response.setStatus(500);
+            e.printStackTrace();
+            return;
         }
         out.println(new Gson().toJson("{message: \"Movies POST was successful\"}"));
         response.setStatus(200);
